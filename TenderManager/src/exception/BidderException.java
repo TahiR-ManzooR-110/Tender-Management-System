27ce23d33981a65e83b2e081b7b04a2198907bd7
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2022-12-15 17:22:37 +0530

    added-Exception

---
+++ TenderManager/src/exception/BidderException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class BidderException extends Exception {
+	
+	public BidderException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BidderException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
