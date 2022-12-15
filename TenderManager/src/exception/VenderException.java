f234f56502c793ad596d51b8143724632f55fde2
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2022-12-15 16:41:58 +0530

    added-Exception

---
+++ TenderManager/src/exception/VenderException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class VenderException extends Exception{
+	
+	public VenderException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public VenderException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
